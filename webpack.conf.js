11fd82da1f3fe21babc0a1356454103c87cfdde3
Matthew Schultz <matthew.schultz@androidpit.de>
2018-04-22 15:44:17 +0930

    Use scss & material.io

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,37 +1,64 @@
-import webpack from "webpack";
-import path from "path";
+import webpack from 'webpack';
+import path from 'path';
 
 export default {
   module: {
     rules: [
       {
-        test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader?name=/[hash].[ext]"
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'css/bundle.css'
+            }
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'sass-loader',
+            options: {
+              importer: function(url, prev) {
+                if (url.indexOf('@material') === 0) {
+                  var filePath = url.split('@material')[1];
+                  var nodeModulePath = `./node_modules/@material/${filePath}`;
+                  return { file: require('path').resolve(nodeModulePath) };
+                }
+                return { file: url };
+              }
+            }
+          }
+        ]
       },
-      {test: /\.json$/, loader: "json-loader"},
       {
-        loader: "babel-loader",
+        test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?name=/[hash].[ext]'
+      },
+      { test: /\.json$/, loader: 'json-loader' },
+      {
+        loader: 'babel-loader',
         test: /\.js?$/,
         exclude: /node_modules/,
-        query: {cacheDirectory: true}
+        query: { cacheDirectory: true }
       }
     ]
   },
 
   plugins: [
     new webpack.ProvidePlugin({
-      "fetch": "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
+      fetch: 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
     })
   ],
 
-  context: path.join(__dirname, "src"),
+  context: path.join(__dirname, 'src'),
   entry: {
-    app: ["./js/app"]
+    app: ['./js/app'],
+    style: './scss/app.scss'
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    publicPath: "/",
-    filename: "[name].js"
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js'
   },
-  externals:  [/^vendor\/.+\.js$/]
+  externals: [/^vendor\/.+\.js$/]
 };
